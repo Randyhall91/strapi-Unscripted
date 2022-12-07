ebf6d777db10098810e7c32e9a0c4a3367f1a2c0
Randyhall91 <randyrocks11@gmail.com>
2022-12-07 08:08:59 -0700

    fix plugin env targets

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('ACCESS_KEY_ID'),
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('REGION'),
+        region: env('AWS_REGION'),
         params: {
           Bucket: env('AWS_BUCKET'),
         },
