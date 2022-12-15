974476c9ef9b48132c6b2893af11f92d982a22c8
Thesommoner <randyrocks11@gmail.com>
2022-12-14 22:02:39 -0700

    change database key syntax

--- config/database.js
+++ config/database.js
@@ -7,11 +7,11 @@ module.exports = ({ env }) => ({
       "connector": "bookshelf",
       "settings": {
         "client": "postgres",
-        "host": env('DATABASE_HOST' || '127.0.0.1'),
-        "port": env('DATABASE__PORT' || 27017),
-        "database": env('DATABASE__NAME'|| 'strapi'),
-        "username": env('DATABASE__USERNAME' || ''),
-        "password": env('DATABASE__PASSWORD'|| '')
+        "host": env('DATABASE_HOST', '127.0.0.1'),
+        "port": env('DATABASE__PORT', 27017),
+        "database": env('DATABASE__NAME', 'strapi'),
+        "username": env('DATABASE__USERNAME', ''),
+        "password": env('DATABASE__PASSWORD', '')
       },
       "options": {
         "ssl": false
